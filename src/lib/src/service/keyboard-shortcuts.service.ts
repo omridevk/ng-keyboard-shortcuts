ca4574aadb64962ee25d1207d5715d431de84c18
Omri Katz <omri.katz@kaltura.com>
2017-09-09 17:35:41 +0300

    support for throttle time

--- src/lib/src/service/keyboard-shortcuts.service.ts
+++ src/lib/src/service/keyboard-shortcuts.service.ts
@@ -1,5 +1,4 @@
 import {
-  Inject,
   Injectable,
   OnDestroy
 }                           from '@angular/core';
@@ -14,62 +13,110 @@ import 'rxjs/add/operator/throttleTime';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 export type Shortcut = {
   key: any;
   command: Function,
+  target?: HTMLElement,
+  preventDefault?: boolean
+};
+
+type ParsedShortcut = {
+  key: Function[],
+  command: Function,
+  preventDefault?: boolean,
   target?: HTMLElement
-}
+};
 
 @Injectable()
 export class KeyboardShortcutsService implements OnDestroy {
+
+
   constructor() {
-    this.subscription = this.keydown$
-      .filter((shortcut: any) => isFunction(shortcut.command))
-      .throttleTime(100)
-      .subscribe(
-        shortcut => shortcut.command(),
-        error => console.error(error)
-      );
+    this.subscription = this.keydown(this.throttleTime).subscribe();
   }
 
-  private shortcuts: Shortcut[] = [];
+  /**
+   * Parsed shortcuts
+   * for each key create a predicate function
+   * { key: Function[], command: func,
+   * @type {Array}
+   */
+  private shortcuts: ParsedShortcut[] = [];
+
+
+  /**
+   * Throttle the keypress event.
+   * @type {number}
+   */
+  private throttleTime: number = 100;
+
+
+  private mapEvent = (shortcuts) => ( event ) =>
+    shortcuts.map(shortcut => Object.assign({}, shortcut, {
+      key: shortcut.key.map((predicate: any) => predicate(event)).every((item) => item),
+      event: event
+    }))
+      .filter(shortcuts => shortcuts.key);
 
   /**
    * Subscription for on destroy.
    */
   private subscription: Subscription;
+
   /**
-   * Key down observable
-   * @type {Observable<any>}
+   * @returns Observable<any>
+   * @param throttleTime
    */
-  private keydown$ = Observable.fromEvent(document, 'keydown')
-    .map(event => this.shortcuts.map(shortcut => {
-      return {
-        keys: shortcut.key.map((predicate: any) => predicate(event)).every((item) => item),
-        command: shortcut.command,
-        target: shortcut.target || document,
-        event: event
-      }
-    }))
-    .map(shortcuts => shortcuts.filter(shortcut => shortcut.keys))
-    .filter((shortcuts:any)=> shortcuts.length)
+  private keydown = (throttleTime: number) => Observable.fromEvent(document, 'keydown')
+    .map(this.mapEvent(this.shortcuts))
+    .filter((shortcuts: any) => shortcuts.length)
     .map(shortcuts => shortcuts.pop())
-    .do(shortcuts => shortcuts.event.preventDefault());
+    .filter(shortcut => !shortcut.target || shortcut.event.target === shortcut.target)
+    .do(shortcut => !shortcut.preventDefault || shortcut.event.preventDefault())
+    .filter((shortcut: any) => isFunction(shortcut.command))
+    .throttleTime(throttleTime)
+    .do(shortcut => shortcut.command())
+    .catch(error => Observable.throw("error in shortcut service"));
 
 
+  /**
+   * Remove subscription.
+   */
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
 
+  /**
+   * Add new shortcut/s
+   * @param {Shortcut[] | Shortcut} shortcuts
+   * @returns {KeyboardShortcutsService}
+   */
   public addShortcuts(shortcuts: Shortcut[] | Shortcut) {
     if (Array.isArray(shortcuts)) {
       shortcuts.forEach((shortcut) => this.shortcuts.push(this.parseCommand(shortcut)));
-      return;
+      return this;
     }
-    this.shortcuts.push(this.parseCommand(shortcuts as Shortcut));
+    this.shortcuts.push(this.parseCommand(shortcuts));
+    return this;
+  }
+
+  /**
+   * Set throttleTime for the keydown event
+   * Default: 100
+   * @param {number} time
+   * @returns {KeyboardShortcutsService}
+   */
+  public setThrottleTime(time: number) {
+    this.throttleTime = time;
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = this.keydown(this.throttleTime).subscribe();
+    return this;
   }
 
   /**
@@ -78,6 +125,7 @@ export class KeyboardShortcutsService implements OnDestroy {
    */
   private getKeys = (command) => command.key.split(' ')
     .map(key => key.trim())
+    .filter(key => key !== '+')
     .map(key => {
       // for modifiers like control key
       // look for event['ctrlKey']
@@ -90,16 +138,14 @@ export class KeyboardShortcutsService implements OnDestroy {
     });
 
   /**
-   * Parse command
-   * and transform it from keys string to key code
-   * using getKeys function.
-   * @param command
-   * @returns Shortcut
+   * Parse each command using getKeys function
+   * @param {Shortcut} command
+   * @returns {ParsedShortcut}
    */
-  private parseCommand = (command: Shortcut) => {
+  private parseCommand(command: Shortcut): ParsedShortcut {
     return Object.assign({}, command, {
-      'key': this.getKeys(command)
+      'key': this.getKeys(command),
     });
-  };
+  }
 
 }
