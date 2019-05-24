fd12fa6a1cb356722b7954073e65225ee5e10325
Omri Katz <omri.katz@kaltura.com>
2019-05-24 18:08:42 +0300

    fix jsdoc

--- projects/ng-keyboard-shortcuts/src/lib/utils.ts
+++ projects/ng-keyboard-shortcuts/src/lib/utils.ts
@@ -1,18 +1,8 @@
-/**
- * @ignore
- * @param x
- * @returns {x is Function}
- */
+
 export function isFunction(x: any): x is Function {
     return typeof x === "function";
 }
 
-/**
- * @ignore
- * @param {Function} fn
- * @param {any[]} list
- * @returns {boolean}
- */
 export const any = (fn: Function, list: any[]) => {
     let idx = 0;
     while (idx < list.length) {
@@ -23,17 +13,13 @@ export const any = (fn: Function, list: any[]) => {
     }
     return false;
 };
-/**
- * ignore
- * @param x
- * @returns {any}
- */
+
 export const identity = x => x;
 
 /**
  * @ignore
  * @param x
- * @returns {boolean}
+ * @returns boolean
  */
 export const isNill = x => x == null;
 
@@ -41,7 +27,7 @@ export const isNill = x => x == null;
  * @ignore
  * @param xs
  * @param key
- * @returns {any}
+ * @returns any
  */
 export const groupBy = (xs, key) =>
     xs.reduce(
@@ -55,9 +41,9 @@ export const groupBy = (xs, key) =>
 
 /**
  * @ignore
- * @param {any[]} first
- * @param {any[]} second
- * @returns {any}
+ * @param first
+ * @param second
+ * @returns any[]
  */
 export const difference = (first: any[], second: any[]) =>
     first.filter(item => !second.includes(item));
@@ -65,7 +51,7 @@ export const difference = (first: any[], second: any[]) =>
 /**
  * @ignore
  * @param preds
- * @returns {(...args) => (boolean)}
+ * @returns (...args) => boolean;
  */
 export const allPass = preds => (...args) => {
     let idx = 0;
@@ -81,7 +67,7 @@ export const allPass = preds => (...args) => {
 
 /**
  * @ignore
- * @returns {any}
+ * @returns string
  */
 export const guid = () =>
     Math.floor((1 + Math.random()) * 0x10000)
