bb6cfb258972258fca3fe5545a745025dc1e029d
omridevk <omrikatz12@gmail.com>
2019-06-21 12:25:02 -0400

    update copy command

--- scripts/copy.js
+++ scripts/copy.js
@@ -3,9 +3,9 @@
 const path = require("path");
 const fs = require("fs-extra");
 const config = require('../angular');
-const readmeFileName = 'README.MD';
+const readmeFileName = 'README.md';
 const project = path.join(__dirname, "../", config.projects["ng-keyboard-shortcuts"].root);
-const root = path.join(__dirname, "../")
+const root = path.join(__dirname, "../");
 const originalReadme = path.join(root, readmeFileName);
 const destReadme = path.join(project, readmeFileName);
 // destination.txt will be created or overwritten by default.
