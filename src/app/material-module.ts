895a2c0f3898bb2083d979d6976db47ba4a0d907
omridevk <omrikatz12@gmail.com>
2019-06-21 12:19:59 -0400

    update readme

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -91,8 +91,3 @@ import {
     ]
 })
 export class MaterialModule {}
-
-
-/**  Copyright 2019 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license */
