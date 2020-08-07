63fbee9ced0a71722ec53370db6b2cb6951c7a47
Omri Katz <omri.katz@kaltura.com>
2020-08-08 01:59:40 +0300

    fix type

--- projects/ng-keyboard-shortcuts/src/lib/ng-keyboard-shortcuts.service.ts
+++ projects/ng-keyboard-shortcuts/src/lib/ng-keyboard-shortcuts.service.ts
@@ -319,7 +319,7 @@ export class KeyboardShortcutsService implements OnDestroy {
     /**
      * @ignore
      */
-    constructor(@Inject(DOCUMENT) private document: Document) {
+    constructor(@Inject(DOCUMENT) private document: any) {
         this.subscriptions.push(
             this.keydownSequence$.subscribe(),
             this.keydownCombo$.subscribe()
