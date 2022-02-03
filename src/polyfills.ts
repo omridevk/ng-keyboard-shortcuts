5f55f510ef609eaf51a233eca6b69a8415f92eb4
Egoist <epsilonelittr@gmail.com>
2022-02-03 17:27:03 +0300

    Update to Angular 12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,7 +56,7 @@ import 'core-js/features/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 
