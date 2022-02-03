959ed92700e969cfd0ac0ef53517d0a7c65d9107
Egoist <epsilonelittr@gmail.com>
2022-02-03 17:34:13 +0300

    Update to Angular 13

--- projects/ng-keyboard-shortcuts/src/lib/ng-keyboard-shortcuts.module.ts
+++ projects/ng-keyboard-shortcuts/src/lib/ng-keyboard-shortcuts.module.ts
@@ -10,7 +10,6 @@ import { CommonModule } from "@angular/common";
 
 @NgModule({
     imports: [CommonModule],
-    entryComponents: [KeyboardShortcutsHelpComponent],
     declarations: [
         KeyboardShortcutsComponent,
         KeyboardShortcutsDirective,
