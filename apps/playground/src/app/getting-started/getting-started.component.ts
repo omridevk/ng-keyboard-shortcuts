7f55ea37a5a785daa4b3fdda8f56334aa9042e00
Omri Katz <omrikatz12@gmail.com>
2022-05-12 20:49:36 +0300

    chore: playground app

---
+++ apps/playground/src/app/getting-started/getting-started.component.ts
@@ -0,0 +1,36 @@
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { ShortcutInput } from "@ng-keyboard-shortcuts/ng-keyboard-shortcuts";
+import { KeyboardShortcutsComponent } from "@ng-keyboard-shortcuts/ng-keyboard-shortcuts";
+import { Subscription } from "rxjs";
+
+@Component({
+    selector: "app-getting-started",
+    templateUrl: "./getting-started.component.html",
+    styleUrls: ["./getting-started.component.css"]
+})
+export class GettingStartedComponent implements OnInit, AfterViewInit, OnDestroy {
+    shortcuts: ShortcutInput[] = [
+        {
+            key: "ctrl + t",
+            description: "ctrl + t",
+            label: "help",
+            command: () => console.log("control + t clicked")
+        }
+    ];
+
+    shortcutsDisabled = false;
+
+    @ViewChild(KeyboardShortcutsComponent, { static: true }) keyboard: KeyboardShortcutsComponent;
+
+    constructor() {}
+
+    ngOnInit() {}
+    subscriptions: Subscription[] = [];
+
+    ngAfterViewInit(): void {
+        this.subscriptions.push(this.keyboard.select("ctrl + t").subscribe((e) => console.log(e)));
+    }
+    ngOnDestroy() {
+        this.subscriptions.forEach((sub) => sub.unsubscribe());
+    }
+}
